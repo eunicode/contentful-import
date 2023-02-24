0b4d7d04ff6e96253e3f410cf61e5e10aae0a403
Eunice Park <28845173+eunicode@users.noreply.github.com>
2023-02-24 06:23:51 -0800

    parse real csv data

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,18 @@
 import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 import contentful from "contentful-management";
 import fs from "fs";
-import { parse as Parser } from "csv-parse";
+// import { parse as Parser } from "csv-parse";
+import { parse } from "csv-parse";
 import path from "path";
 import { fileURLToPath } from "url";
+import { pipeline } from "stream";
 
 dotenv.config();
-
-// Look at PlainClientAPI type for hints
 
 // With scoped space and environment
 const scopedPlainClient = contentful.createClient(
   {
-    // This is the access token for this space. Normally you get the token in the Contentful web app
+    // This is the access token for this space. Can get the token in the Contentful web app
     accessToken: process.env.PERSONAL_TOKEN,
   },
   {
@@ -24,49 +24,77 @@ const scopedPlainClient = contentful.createClient(
   }
 );
 
-// entries from '<space_id>' & '<environment_id>'
-const entries = await scopedPlainClient.entry.getMany({
-  query: {
-    skip: 5,
-    limit: 10,
-  },
-});
-
-// console.log(entries.items);
-
-const singleEntry = await scopedPlainClient.entry.get({
-  entryId: "19",
-});
-
-// console.log(singleEntry);
+// If you use Node.js version 16.12.0 or more recent, you can use top-level await
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename); // /Users/someuser/contentful-import/scripts
-const csvPath = path.join(__dirname, "../data/sample-faq.csv");
+const __dirname = path.dirname(__filename); // returns /Users/someuser/contentful-import/scripts
 
-const parser = new Parser({ delimiter: ",", columns: true });
-const readStream = fs.createReadStream(csvPath).pipe(parser);
-rowToEntry("nothing");
-// for await (const chunk of readStream) {
-//   // console.log(chunk);
-//   const entry = await rowToEntry(chunk);
-//   console.log(entry);
-//   entry.publish();
-// }
+// ARGUMENT: CHANGE FILE PATH
+const csvPath = path.join(__dirname, "../data/report-230222-1677111566139.csv");
+// const csvPath = path.join(__dirname, "../data/sample-faq.csv");
 
-async function rowToEntry(row) {
-  scopedPlainClient.entry.create(
-    { contentTypeId: "faq" },
-    {
-      fields: {
-        id: { "en-US": 999999 },
-        answer: {
-          "en-US": "This is an answer",
-        },
-        question: {
-          "en-US": "This is a question",
-        },
-      },
-    }
-  );
+// Initialize the csv parser
+const parser = parse({
+  delimiter: ",",
+  columns: true,
+  relax_quotes: true,
+});
+
+// Convert CSV to JSON
+const readableStream = fs.createReadStream(csvPath).pipe(parser);
+
+for await (const chunk of readableStream) {
+  const entry = await rowToEntry(chunk);
+  console.log(entry);
+  // entry.publish();
 }
+
+function extractUrl(html) {
+  let url = html.replaceAll(/<[^>]*>/, "");
+  if (url.startsWith("https://www.expertise.com/")) {
+    return url;
+  } else {
+    console.log("Error extracting url");
+  }
+}
+
+async function rowToEntry({
+  "Snippet ID": id,
+  "Directory URL": url,
+  "Standard Vertical Q1": Q1v,
+  "Vertical Answer 1 - Content": A1v,
+  "Standard Vertical Q2": Q2v,
+  "Vertical Answer 2 - Content": A2v,
+  "Question 1- Content": Q1c,
+  "Answer  1 - Content": A1c,
+  "Question 2 - Content": Q2c,
+  "Answer  2 - Content": A2c,
+  "Question 3 - Content": Q3c,
+  "Answer  3 - Content": A3c,
+  "Question 4 - Content": Q4c,
+  "Answer  4 - Content": A4c,
+  "Question 5 - Content": Q5c,
+  "Answer  5 - Content": A5c,
+  "Last Modified DT": lastMod,
+}) {
+  console.log(A2v);
+
+  // scopedPlainClient.entry.create(
+  //   { contentTypeId: "faq" },
+  //   {
+  //     fields: {
+  //       id: { "en-US": 999999 },
+  //       answer: {
+  //         "en-US": "This is an answer",
+  //       },
+  //       question: {
+  //         "en-US": "This is a question",
+  //       },
+  //     },
+  //   }
+  // );
+}
+
+/* 
+
+*/
---
+++ scripts/pipeline.js
@@ -0,0 +1,114 @@
+import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import contentful from "contentful-management";
+import fs from "fs";
+// import { parse as Parser } from "csv-parse";
+import { parse } from "csv-parse";
+import path from "path";
+import { fileURLToPath } from "url";
+import { pipeline } from "stream";
+
+dotenv.config();
+
+// Look at PlainClientAPI type for hints
+
+// With scoped space and environment
+const scopedPlainClient = contentful.createClient(
+  {
+    // This is the access token for this space. Normally you get the token in the Contentful web app
+    accessToken: process.env.PERSONAL_TOKEN,
+  },
+  {
+    type: "plain",
+    defaults: {
+      spaceId: process.env.SPACE_ID,
+      environmentId: process.env.ENVIRONMENT_ID,
+    },
+  }
+);
+
+// If you use Node.js version 16.12.0 or more recent, you can use top-level await
+// entries from '<space_id>' & '<environment_id>'
+// const entries = await scopedPlainClient.entry.getMany({
+//   query: {
+//     skip: 5,
+//     limit: 10,
+//   },
+// });
+
+// console.log(entries.items);
+
+// const singleEntry = await scopedPlainClient.entry.get({
+//   entryId: "19",
+// });
+
+// console.log(singleEntry);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// returns /Users/someuser/contentful-import/scripts
+
+// ARGUMENT: CHANGE FILE PATH
+const csvPath = path.join(__dirname, "../data/report-230222-1677111566139.csv");
+
+// Convert CSV to JSON
+// const parser = new Parser({ delimiter: ",", columns: true });
+
+// Initialize the csv parser
+const parser = parse({
+  delimiter: ",",
+  columns: true,
+  relax_quotes: true,
+});
+
+// const readableStream = fs
+//   .createReadStream(csvPath)
+//   .on("error", () => console.log("error with file path"))
+//   .pipe(parser);
+
+// Use stream to consume chunks
+const csvStream = pipeline(fs.createReadStream(csvPath), parser, (err) => {
+  if (err) {
+    console.error("Pipeline failed", err);
+  } else {
+    console.log("Pipeline succeeded");
+  }
+});
+
+async function chunkWork(readable) {
+  for await (const chunk of readable) {
+    //  rowToEntry()
+    console.log({
+      row: chunk.Name,
+    });
+  }
+}
+
+// chunkWork(csvStream);
+// undefined gets logged, then 'Pipeline succeeded'
+for await (const chunk of csvStream) {
+  console.log(chunk.Name);
+  // const entry = await rowToEntry(chunk);
+  // console.log(entry);
+  // entry.publish();
+}
+
+async function rowToEntry(row) {
+  scopedPlainClient.entry.create(
+    { contentTypeId: "faq" },
+    {
+      fields: {
+        id: { "en-US": 999999 },
+        answer: {
+          "en-US": "This is an answer",
+        },
+        question: {
+          "en-US": "This is a question",
+        },
+      },
+    }
+  );
+}
+
+/* 
+
+*/
